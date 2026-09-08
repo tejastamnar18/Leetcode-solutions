e606ab0b7308617257f49b923e697114f3ba508d
Tejas Tamnar <tejastamnar8034@gmail.com>
2026-09-08 06:38:10 +0530

    Time: 1 ms (83.65%), Space: 42.2 MB (93.72%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,8 +1,8 @@
 class Solution {
     public int countCommas(int n) {
-        if (n >= 1000) {
-           return n - 999;
+        if (n < 1000) {
+            return 0;
         }
-        return 0;
+        return n - 999;
     }
 }
