776df2c85cc6e2f6f4b1f1ae760e9c7a9ea26bb8
Tejas Tamnar <tejastamnar8034@gmail.com>
2026-09-17 03:24:59 +0530

    Time: 0 ms (100%), Space: 42 MB (70.35%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,17 +1,14 @@
 class Solution {
     public int fib(int n) {
-        int arr[] = new int[n+1];
-        Arrays.fill(arr,-1);
-        return fibDB(arr,n);
-    }
-
-    public int fibDB(int[] arr,int n){
         if(n<=1){
             return n;
         }
-        if(arr[n] != -1){
-            return arr[n];
+        int arr[] = new int[n + 1];
+        arr[0] = 0;
+        arr[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
         }
-        return arr[n] = fibDB(arr,n-1)+fibDB(arr,n-2);
+        return arr[n];
     }
 }
